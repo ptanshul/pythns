220c5ceab312927f9b44caca66c233cc40fffc16
ptanshul <anshul.tiwari@rediffmail.com>
2023-10-31 23:16:27 +0530

    common 1

---
+++ common.py
@@ -0,0 +1,7 @@
+def common_elements(list1, list2):
+    return list(set(list1) & set(list2))
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+common = common_elements(list1, list2)
+print(f"Common elements: {common}")
