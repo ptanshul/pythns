236026da9ad53a0593f6fd5bfb315033bf8ac2a6
ptanshul <anshul.tiwari@rediffmail.com>
2023-11-03 00:38:25 +0530

    item count in list1

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+my_list = [1, 2, 2, 3, 4, 2, 5, 2,2,2,2,2,2,2,2]
+item_to_count = 2
 
+count = my_list.count(item_to_count)
+print(f"{item_to_count} appears {count} times in the list.")
