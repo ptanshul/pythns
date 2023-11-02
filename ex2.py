05c771e5af966f5a5231aad0e8c489e38154c03f
ptanshul <anshul.tiwari@rediffmail.com>
2023-11-03 01:00:16 +0530

    python set

---
+++ ex2.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+union_result = set1 | set2  # Union of two sets
+intersection_result = set1 & set2  # Intersection of two sets
+difference_result = set1 - set2  # Difference of two sets
---
+++ ex3.py
@@ -0,0 +1,9 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+union_result = set1 | set2  # Union of two sets
+intersection_result = set1 & set2  # Intersection of two sets
+difference_result = set1 - set2  # Difference of two sets
+print(union_result)
+print(intersection_result)
+print(difference_result)
